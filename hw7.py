fbf67bfb920c327adbe8ebca7dcf18556cb2bf50
Oleg Rubanov <165693738+orubanov@users.noreply.github.com>
2024-04-10 02:35:01 +0300

    Add files via upload

---
+++ hw7.py
@@ -0,0 +1,16 @@
+my_list = ['banana','cherry','apple','orange','pear','hat']
+print('Список:', my_list)
+print('Первый элемент:', my_list[0])
+print('Последний элемент:', my_list[-1])
+print('Элементы с третьего до пятый: ', my_list[2:5]) #надеюсь что в задание имелось ввиду включительно
+my_list[2]='cat'
+print('Изменёный список:', my_list)
+
+
+
+my_dict = {'banana':'банан','cherry':'вишня','apple':'компания'}
+print('Изначальный словарь:', my_dict)
+print('Элемент с ключом <<apple>>:', my_dict['apple'])
+my_dict['apple'] = 'яблоко'
+my_dict['orange'] = 'апельсин'
+print('Изменённый словарь:', my_dict)
